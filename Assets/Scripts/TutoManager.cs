b2cc2bc0438f8eb5c39328ff23423ecb5c7ee20b
PawnCode <nathpros@hotmail.fr>
2018-04-22 21:35:20 +0100

    Debut tuto

---
+++ Assets/Scripts/TutoManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutoManager : MonoBehaviour {
+
+    public string[] allTextTuto;
+
+    public Text tutoText;
+
+    private int indexTuto;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void NextPhase()
+    {
+
+    }
+
+
+}
